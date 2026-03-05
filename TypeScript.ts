deecd546ee59f7dc8bc5ee9885474d19a10792dd
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-05 17:03:42 +0500

    Add TypeScript variable types examples to documentation

--- TypeScript.ts
+++ TypeScript.ts
@@ -5,3 +5,15 @@
 2. TypeScript adds static typing and other features to the language.
 3. It was developed by microsoft
 */
+
+
+// Types of TypeScript
+
+let str: string = "TypeScript"
+let num: number = 123456
+let bool: boolean = true
+let undi: undefined = undefined
+let nul: null = null
+let unkn: unknown = 251654
+let any: any = 5415
+
