4a821e5f6290354f4d907c3e13dc8b2ece4178f0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-06 20:40:22 +0500

    Add readonly cities array in ArrayEnum.ts to define a list of cities, demonstrating immutability in TypeScript. Commented out examples illustrate the non-mutable nature of the array.

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -10,3 +10,8 @@ const menu: Chai[] = [
     { name: "Masala Chai", price: 300 },
     { name: "Adrak Chai", price: 200 }
 ]
+
+// readonly array dose not update/change
+const cities: readonly string[] = ["Karachi", "Lahore", "Peshawar"]
+// cities.push("Islamabad")  // not working
+// console.log(cities);
