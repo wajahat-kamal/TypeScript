ed5c5a9aff785f9c299b41af2d343b8db041e70c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-06 20:45:38 +0500

    Add tuple and readonly examples in ArrayEnum.ts to demonstrate TypeScript features, including a location tuple and comments on immutability for clarity.

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -1,11 +1,10 @@
+// Arrays, Tuples and Enums
 
 // array of objects
-
 type Chai = {
     name: string,
     price: number
 }
-
 const menu: Chai[] = [
     { name: "Masala Chai", price: 300 },
     { name: "Adrak Chai", price: 200 }
@@ -15,3 +14,9 @@ const menu: Chai[] = [
 const cities: readonly string[] = ["Karachi", "Lahore", "Peshawar"]
 // cities.push("Islamabad")  // not working
 // console.log(cities);
+
+
+// tuple
+const location: [string, number] = ["Karachi", 200]
+
+// readonly tuple
