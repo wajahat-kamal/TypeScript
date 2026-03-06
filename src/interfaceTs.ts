5d52a203fec75a1d8d1bb790faac398fa47df2d6
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-06 20:26:26 +0500

    Add Cup type definition in interfaceTs.ts to represent cup sizes, and create smallCup and largeCup instances for improved type representation.

--- src/interfaceTs.ts
+++ src/interfaceTs.ts
@@ -17,3 +17,11 @@ interface Config {
 const config1: Config = { appName: "Youtube", version: 1 }
 // config1.appName = "cursor" // cannot change because its readonly
 console.log(config1);
+
+
+// 
+type Cup = { size: string }
+let smallCup: Cup = { size: "200ml" }
+let largeCup = { size: "200ml", material: "steel" }
+
+smallCup = largeCup
