6a71a53677e8a9590a49897bb8a4a3fdaff9f971
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-06 20:36:17 +0500

    Add Chai type definition and menu array in ArrayEnum.ts to represent chai options with names and prices, enhancing type safety and structure.

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -1,2 +1,12 @@
 
 // array of objects
+
+type Chai = {
+    name: string,
+    price: number
+}
+
+const menu: Chai[] = [
+    { name: "Masala Chai", price: 300 },
+    { name: "Adrak Chai", price: 200 }
+]
