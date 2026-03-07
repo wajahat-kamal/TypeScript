62b2c477fb858d654660744d96669d76f43c0301
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:47:18 +0500

    Refactor WebReq.ts to log product details (title, brand, price) for the first 10 products from the API response, improving output clarity.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -2,5 +2,9 @@ import axios from "axios";
 
 
 axios.get("https://dummyjson.com/products")
-.then(res => console.log(res.data.products.splice(0, 10)))
+.then(res => {
+    for (const product of res.data.products.splice(0,10)) {
+        console.log({title: product.title, brand: product.brand, price: product.price})
+    }
+})
 .catch(err => console.log(err))
