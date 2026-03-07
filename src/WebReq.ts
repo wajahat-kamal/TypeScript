9db81448e5817be7c324405e97fffb84e62559b0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:47:46 +0500

    Refactor WebReq.ts to use await for axios request, improving readability and maintaining error handling for logging product details.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 
-axios.get("https://dummyjson.com/products")
-.then(res => {
-    for (const product of res.data.products.splice(0,10)) {
-        console.log({title: product.title, brand: product.brand, price: product.price})
-    }
-})
-.catch(err => console.log(err))
+await axios.get("https://dummyjson.com/products")
+    .then(res => {
+        for (const product of res.data.products.splice(0, 10)) {
+            console.log({ title: product.title, brand: product.brand, price: product.price })
+        }
+    })
+    .catch(err => console.log(err))
