ecc6e8054ce6f40483c7207b39bbbeca73527ac0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:59:30 +0500

    Update Shoe interface in WebReq.ts to include an id property and refactor product logging for improved clarity.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -1,8 +1,9 @@
 import axios, { type AxiosResponse } from "axios";
 
 interface Shoe {
-    title: string,
-    brand: string,
+    id: number
+    title: string
+    brand: string
     price: number
 }
 
@@ -11,12 +12,13 @@ interface Shoe {
     try {
         const res: AxiosResponse = await axios.get("https://dummyjson.com/products")
         for (const product of res.data.products.splice(0, 10)) {
-            const res: Shoe = {
+            const data: Shoe = {
+                id: product.id,
                 title: product.title,
                 brand: product.brand,
                 price: product.price
             }
-            console.log(res)
+            console.log(data)
         }
     } catch (error) {
         console.log(error);
