68335d8d330b04eec8b0f38612de01e9c9f0f46b
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:44:20 +0500

    Update WebReq.ts to log the first 10 products from the API response and add error handling for the axios request.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -2,4 +2,5 @@ import axios from "axios";
 
 
 axios.get("https://dummyjson.com/products")
-.then(res => console.log(res))
+.then(res => console.log(res.data.products.splice(0, 10)))
+.catch(err => console.log(err))
