3687080ae76a0014d845fc085079d5f0981c16ef
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:52:16 +0500

    Add Shoe interface in WebReq.ts to improve type safety for product details in API response.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-
+interface Shoe {
+    title: string,
+    brand: string,
+    price: number
+}
 
 await axios.get("https://dummyjson.com/products")
     .then(res => {
         for (const product of res.data.products.splice(0, 10)) {
-            const res = {
+            const res: Shoe = {
                 title: product.title,
                 brand: product.brand,
                 price: product.price
