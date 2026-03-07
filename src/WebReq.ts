95730dfef764fea673ad292aeeb14dbb2a6e61b5
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-07 21:50:56 +0500

    Refactor WebReq.ts to structure product logging into an object for improved clarity and maintainability.

--- src/WebReq.ts
+++ src/WebReq.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 
+
 await axios.get("https://dummyjson.com/products")
     .then(res => {
         for (const product of res.data.products.splice(0, 10)) {
-            console.log({ title: product.title, brand: product.brand, price: product.price })
+            const res = {
+                title: product.title,
+                brand: product.brand,
+                price: product.price
+            }
+            console.log(res)
         }
     })
     .catch(err => console.log(err))
